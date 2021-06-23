bb5f6deb1a8fdc2357a89f3197d9eea2a488aed2
madwayz <djurball@list.ru>
2021-06-24 03:16:15 +0700

    * Пофиксил кнопку "Назад" в конфигураторе чатов * Пофиксил обновление базового стейта. Не будет пытаться его обновить, если нет нужных колбэк-данных

--- app/bot/base/helpers.py
+++ app/bot/base/helpers.py
@@ -3,6 +3,9 @@ from bot.utils.account_type import get_account_type_id
 
 async def update_base_state(state, callback_query):
     account_type_data = get_account_type_id(callback_query.data)
+    if not account_type_data:
+        return
+
     account_type = account_type_data.get('id')
     account_type_alias = account_type_data.get('alias')
 
--- app/bot/utils/account_type.py
+++ app/bot/utils/account_type.py
@@ -1,8 +1,9 @@
-def get_account_type_id(account_type):
+def get_account_type_id(callback_data):
     type_id_schema = [
         {'callbacks_data': ['taxi_delivery_settings', 'taxi_account'], 'data': {'id': 0, 'alias': 'HR'}},
         {'callbacks_data': ['invest_delivery_settings', 'invest_account'], 'data': {'id': 1, 'alias': 'Инвест'}}
     ]
 
-    response = list(filter(lambda item: account_type in item['callbacks_data'], type_id_schema))[0]
-    return response.get('data')
+    response = list(filter(lambda item: callback_data in item['callbacks_data'], type_id_schema))
+    return response[0].get('data') if response else None
+
